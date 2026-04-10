1edd1a6ac7ae2a63ef4563120dcb9a59b8e10330
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-04-10 15:11:49 -0300

    Modo restrito
    :)

---
+++ aula02.js
@@ -0,0 +1,10 @@
+"use strict" // Modo restrito: impede erros silenciosos e evita práticas ruins, como usar variáveis sem declarar
+
+let melhorAlimentoDoMundo = "Batata" // Declarando uma variável com valor do tipo string
+
+console.log("Apenas uma string") // Exibindo uma string diretamente
+
+console.log(melhorAlimentoDoMundo) // Exibindo o valor atribuído à variável
+
+console.log("Um dos melhores alimentos do mundo: " + melhorAlimentoDoMundo) 
+// Concatenando uma string com uma variável (juntando os dois valores)
